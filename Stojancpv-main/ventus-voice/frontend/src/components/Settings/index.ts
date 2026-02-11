a96f6d9ddbb5009fe06ff9a8b1bee1562fa70d7b
stefanstojanovicstefa-creator <stefanstojanovicstefa@gmail.com>
2026-02-11 01:28:39 +0100

    Delete Stojancpv-main/ventus-voice directory

--- Stojancpv-main/ventus-voice/frontend/src/components/Settings/index.ts
+++
@@ -1,6 +0,0 @@
-export { GeneralSettings } from './GeneralSettings';
-export { ProfileSettings } from './ProfileSettings';
-export { AppearanceSettings } from './AppearanceSettings';
-export { ApiKeySettings } from './ApiKeySettings';
-export { NotificationSettings } from './NotificationSettings';
-export { SecuritySettings } from './SecuritySettings';
